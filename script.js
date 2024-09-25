08f805fe21bd0896f1304a61679932d4c4357299
Brayan Ceballos <brayanceballos08@gmail.com>
2024-09-25 17:00:56 -0400

    Modificaciones clase 8.3

---
+++ script.js
@@ -0,0 +1,17 @@
+let persona = {
+    name: "Pedrito",
+    lastname: "Angulo",
+    age: 27,
+    adress:{
+        city: "Tucupita",
+        country: "Mexico",
+        street: "main street",
+    }
+}
+
+let cancion={
+    id:"4g5fs1s4f5",
+    title: "BIRDS OF A FEATHER",
+    author: "BILLIE EILISH",
+    album: "LAST ALBUM",
+}
